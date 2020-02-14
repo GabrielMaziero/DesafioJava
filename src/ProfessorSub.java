c8a2cb0993c842a665b8d173575f6f68706666a6
Treinamento <Treinamento@RD723904.raiadrogasil.com>
2020-02-14 11:43:03 -0300

    Parte D do desafio feita.

---
+++ src/ProfessorSub.java
@@ -0,0 +1,12 @@
+public class ProfessorSub extends Professor {
+
+    private Integer qtdHoras;
+
+    public Integer getQtdHoras() {
+        return qtdHoras;
+    }
+
+    public void setQtdHoras(Integer qtdHoras) {
+        this.qtdHoras = qtdHoras;
+    }
+}
