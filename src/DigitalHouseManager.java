f4bf94e9b53727f9516bb0f88bae7e519f5dea14
Treinamento <Treinamento@RD723904.raiadrogasil.com>
2020-02-14 12:29:15 -0300

    Parte G do desafio feita.

---
+++ src/DigitalHouseManager.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class DigitalHouseManager {
+
+    private List<Aluno> listaDeAlunos;
+    private List<Professor> listaDeProfessores;
+    private List<Curso> listaDeCursos;
+    private List<Matricula> listaDeMatricula;
+    
+}
